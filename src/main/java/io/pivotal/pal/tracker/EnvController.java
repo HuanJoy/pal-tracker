ce988b1bd0f523525397895645c7084b16282fb0
Tyson Gern <tgern@pivotal.io>
2017-07-20 13:56:50 -0600

    Add tests for deployment lab

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Value("${WELCOME_MESSAGE}")
+    private String welcomeMessage;
+
+
+    @GetMapping("/")
+
+    public String sayHello() {
+        return this.welcomeMessage;
+    }
+}
