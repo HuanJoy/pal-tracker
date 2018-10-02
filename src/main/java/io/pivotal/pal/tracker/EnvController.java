36b24df8ea173acfdd7c1fb5c9823b3e8deaccd0
Huan Liu <hliu2@ford.com>
2018-10-02 08:46:29 -0600

    pipeline changes

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,19 +1,32 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 @RestController
-public class WelcomeController {
+public class EnvController {
 
-    @Value("${WELCOME_MESSAGE}")
-    private String welcomeMessage;
+    private Map<String, String> env = new HashMap<String, String>();
 
-
-    @GetMapping("/")
-
-    public String sayHello() {
-        return this.welcomeMessage;
+    @Autowired
+    public EnvController(@Value("${PORT:NOT SET}")String port, @Value("${MEMORY_LIMIT:NOT SET}")String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}")String instance, @Value("${CF_INSTANCE_ADDR:NOT SET}")String address) {
+        this.env.put("PORT", port);
+        this.env.put("MEMORY_LIMIT", memoryLimit);
+        this.env.put("CF_INSTANCE_INDEX", instance);
+        this.env.put("CF_INSTANCE_ADDR", address);
     }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+        return this.env;
+    }
+
 }
